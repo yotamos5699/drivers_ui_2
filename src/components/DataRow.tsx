edf8d4ffe8230d0df43f827f61877d4a1d9b0d8c
yotamos5699 <m.lhafakot@gmail.com>
2022-12-13 14:12:01 +0200

    -navigate

--- src/components/DataRow.tsx
+++ src/components/DataRow.tsx
@@ -1,9 +1,16 @@
 import { CSS } from "@dnd-kit/utilities";
 import { BiNavigation, BiDetail } from "react-icons/bi";
 import { useSortable } from "@dnd-kit/sortable";
-import { AiOutlinePhone } from "react-icons/ai";
+import { AiOutlineMessage, AiOutlinePhone } from "react-icons/ai";
 export default function DataRow(props: any) {
-  const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({ id: props.id });
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    // isDragging,
+  } = useSortable({ id: props.id });
   const style = {
     transition,
     transform: CSS.Transform.toString(transform),
@@ -11,9 +18,19 @@ export default function DataRow(props: any) {
     //:center,
   };
 
+  //   url = q=Hawaii";
+  //   Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+  //   startActivity(intent);
+  // } catch (ActivityNotFoundException ex) {
+  //   // If Waze is not installed, open it in Google Play:
+  //   Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.waze"));
+  //   startActivity(intent);
+  // }
   return (
     <div
-      className={"flex flex-col items-center px-2 py-4 w-4/5 bg-gray-100 shadow-md rounded-md gap-1 touch-none"}
+      className={
+        "flex flex-col items-center px-2 py-4 w-4/5 bg-gray-100 shadow-md rounded-md gap-1 touch-none"
+      }
       ref={setNodeRef}
       id={props.id}
       {...attributes}
@@ -37,15 +54,21 @@ export default function DataRow(props: any) {
         </a>
       </div>
       <div className="flex gap-2">
-        <span className="font-bold"> כתובת </span>
+        <a
+          href={`"https://waze.com/ul?q=${new URLSearchParams(
+            props.row["כתובת"]
+          )}`}
+        >
+          <span className="font-bold"> כתובת </span>
 
-        <span> {props.row["כתובת"]} </span>
-        <span className="font-bold" id={"navigate"}>
-          נווט
-        </span>
-        <span className="items-center" id={"navigate"}>
-          <BiNavigation className="font-bold" id={"navigate"} />{" "}
-        </span>
+          <span> {props.row["כתובת"]} </span>
+          <span className="font-bold" id={"navigate"}>
+            נווט
+          </span>
+          <span className="items-center" id={"navigate"}>
+            <BiNavigation className="font-bold" id={"navigate"} />{" "}
+          </span>
+        </a>
       </div>
       <div className="flex gap-2" id={"details"}>
         <span className="font-bold" id={"details"}>
@@ -68,8 +91,25 @@ export default function DataRow(props: any) {
             checked={props.row["isDone"]}
           />
         </span>
+        {/* <span>
+        <AiOutlineMessage
+          className={`icn1${props.row["msg"]&& "text-green-400"}`}
+          id="msg"
+         
+          //   value={props.row["isDone"]}
+          onChange={(e) => props.handleDragEnd(e, props)}
+       
+        
+        />
+
+        </span> */}
       </div>
       {/* </td> */}
     </div>
   );
 }
+
+// export const messageBox =(props:any)=>{
+//       return <input type={"text"}>
+//       </input>
+// }
